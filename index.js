77ea830f5e98bfe4ee6b79b1caeaa2b902c85043
luigiboy67 <powercall9@gmail.com>
2018-02-20 02:42:42 +0000

    worked on sending html files

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 var express = require("express");
 var app = express();
-var port = 3000;
+var port = process.env.PORT || 3000;
+
+app.use('/*', (req, res, next) => {
+    console.log(`request @ ${req.baseUrl}`);
+    next();
+})
 
 app.get("/", function(req,res) {
     console.log('Got a request, replying...');
-    res.send('I am hungry. Feed me food Michael.\n');
+    res.sendFile(`${__dirname}/index.html`);
     console.log("We have recieved your request.");
 });
 
 app.listen(port, function() {
-    console.log("The server has start on port number: " + port);
+    console.log(`The server has start on port number: ${port}`);
 });
